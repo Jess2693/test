f5ae5cce28069046e71a0af8fdf39c85c4615d4b
Jessenia <jessalexandra999@gmail.com>
2022-12-28 17:07:27 -0500

    add update component

--- frontend/notes-frontend/src/app/update-note/update-note.component.ts
+++ frontend/notes-frontend/src/app/update-note/update-note.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,OnInit  } from '@angular/core';
+import { Note } from '../note';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NoteService } from '../note.service';
 
 @Component({
   selector: 'app-update-note',
   templateUrl: './update-note.component.html',
   styleUrls: ['./update-note.component.css']
 })
-export class UpdateNoteComponent {
+export class UpdateNoteComponent implements OnInit {
+
+  id:number;
+  note:Note = new Note();
+  constructor(private noteService:NoteService,private router:Router,private route:ActivatedRoute) { }
+
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.noteService.getNoteById(this.id).subscribe(data =>{
+      this.note=data;
+    },error => console.log(error));
+  }
+
+  goToNotesList(){
+    this.router.navigate(['/notes']);
+
+  }
+  onSubmit(){
+    this.noteService.updateNote(this.id,this.note).subscribe(dato => {
+      this.goToNotesList();
+    },error => console.log(error));
+  }
+
 
 }
